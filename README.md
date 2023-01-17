079aa6c69336cd069e98dd086bfc7843e08eb934
Tanja Schmidt <tanja@tanjasportfolio.com>
2023-01-17 22:17:02 +0100

    add contract setup incl. deployment

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Have Node.js installed on your computer. This project was developed using Node.j
 
 Have the Whitelist contract deployed on the network you want to use. See the [Whitelist contract]() for more details.
 
-
+Set up a goerli endpoint on Quicknode. See the [Quicknode documentation](https://www.quicknode.com/guides/ethereum/quicknode-ethereum-rpc-endpoints/) for more details. Of course, you can use any other Ethereum endpoint instead.
 ### Installing Dependencies for the Blockchain part
 
 ```bash
@@ -25,3 +25,10 @@ npm install @openzeppelin/contracts
 npm install dotenv
 
 ```
+Add private key and quicknode endpoint to .env file. See the [.env-example file](hardhat-tutorial/.env-example) for more details.
+Run:
+```bash
+npx hardhat compile
+npx hardhat run scripts/deploy.js --network goerli
+```
+
--- blockchain-pt/scripts/deploy.js
+++ blockchain-pt/scripts/deploy.js
@@ -1,31 +1,38 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+    // Address of the whitelist contract that you deployed in the previous module
+    const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+    // URL from where we can extract the metadata for a Crypto Dev NFT
+    const metadataURL = METADATA_URL;
+    /*
+    A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+    so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
+    */
+    const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+    // deploy the contract
+    const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
+        metadataURL,
+        whitelistContract
+    );
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    // Wait for it to finish deploying
+    await deployedCryptoDevsContract.deployed();
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+    // print the address of the deployed contract
+    console.log(
+        "Crypto Devs Contract Address:",
+        deployedCryptoDevsContract.address
+    );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// Call the main function and catch if there is any error
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
