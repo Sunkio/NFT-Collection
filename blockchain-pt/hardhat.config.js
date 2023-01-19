8f8d6e3cbd9597a11622779a9755a6796d3ab397
Tanja <tanja@pop-os.localdomain>
2023-01-19 12:37:20 +0100

    remove ; from .env-example neither compilation nor deployment work otherwise

--- blockchain-pt/hardhat.config.js
+++ blockchain-pt/hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 
 const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const PRIVATE_KEY = `0x${process.env.PRIVATE_KEY}`;
 
 module.exports = {
   solidity: "0.8.4",
@@ -12,4 +12,4 @@ module.exports = {
       accounts: [PRIVATE_KEY],
     },
   },
-};
+}
